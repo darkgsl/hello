0a1b4be806bfaa5e227ad6b9e020ef86e08dcc30
Sergey Gomolko <darkgsl@gmail.com>
2026-01-14 13:23:55 +0000

    uwsgi version

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ def do_magic():
   now = datetime.datetime.now()
   return "Hello! {0}".format(now)
 
+def application(env, start_response):
+  start_response('200 OK', [('Content-Type', 'text/html')])
+  return [do_magic()]
+
 if __name__ == "__main__":
   print(do_magic())

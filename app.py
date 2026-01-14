cd9c56ae61e3c8d271e86f5a8144a6fabe0d24be
Sergey Gomolko <darkgsl@gmail.com>
2026-01-14 20:27:37 +0000

    update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 import datetime
+import socket
 
 def do_magic():
   now = datetime.datetime.now()
-  return "Hello! {0}".format(now)
+  hostname = socket.gethostname()
+  return "Hello  from ->  {0} <- ! {1}".format(hostname, now)
 
 def application(env, start_response):
   start_response('200 OK', [('Content-Type', 'text/html')])
